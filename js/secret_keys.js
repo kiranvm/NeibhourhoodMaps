fb28794f9b2b309db26d4fec80f5bee22aa44a9a
Kiran Vadakkath <kiranvadakath@gmail.com>
2018-07-15 01:41:27 +0530

    removing gitignore for pushing in github pages

---
+++ js/secret_keys.js
@@ -0,0 +1,12 @@
+var keys = {
+        googleMapsKey: 'AIzaSyC7-_x_zVE_3B-gu9qx5ukAhQLEJVaooMo',
+        foursquareClientId: 'B4PNVNPN53W4NECAK1JY0VF3WJTDVRMAKQ1B4USDRMDAVHY3',
+	foursquareClientSecret: 'W5O2SKHRPTJZMR0Z0KIXN0SEWB345CGHI5UDNQ34OEMOCIMB'
+};
+
+var map_secret=function(){
+   var keys = {
+        key1:"AIzaSyC7-_x_zVE_3B-gu9qx5ukAhQLEJVaooMo"
+    };
+   return textMultiple;
+}
