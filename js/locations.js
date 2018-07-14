07e5c43dee3e764e1245f37a1e9fd049aa77f480
Kiran Vadakkath <kiranvadakath@gmail.com>
2018-07-15 01:14:39 +0530

    Full and final changes

---
+++ js/locations.js
@@ -0,0 +1,53 @@
+/* Model Data */
+
+var locations = [
+    {
+        title: 'Taj Mahal',
+        location: {
+            lat: 27.1746278,
+            lng: 78.0416659
+        }
+    },
+    {
+        title: 'Qutub Minar',
+        location: {
+            lat: 28.524467,
+            lng: 77.18546
+        }
+    },
+    {
+        title: 'Vitthala Temple, Hampi',
+        location: {
+            lat: 15.34229,
+            lng: 76.475867
+        }
+    },
+    {
+        title: 'Rani ki Vav (Queens Stepwell), Gujarat',
+        location: {
+            lat: 23.8589192,
+            lng: 72.1018997
+        }
+    },
+    {
+        title: 'Golden Temple, Amritsar',
+        location: {
+            lat: 31.622036,
+            lng: 74.875909
+        }
+    },
+    {
+        title: 'Khajuraho Group of Temples',
+        location: {
+            lat: 24.852928,
+            lng: 79.920022
+        }
+    },
+    {
+	title: 'Ellora Caves',
+	location: {
+	    lat: 20.023777,
+	    lng: 75.178774
+	}
+    }
+];
